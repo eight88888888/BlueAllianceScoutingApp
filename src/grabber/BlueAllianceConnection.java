c50b9405125e605aa00e1f1a483ce77ac5fb3b8c
Ty Williams <tywill8888@gmail.com>
2017-04-19 19:02:44 -0400

    Made a master method connection method, and more specific ones that utilize the master method

--- src/grabber/BlueAllianceConnection.java
+++ src/grabber/BlueAllianceConnection.java
@@ -1,32 +1,43 @@
 package grabber;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BlueAllianceConnection {
-	final String urlBegining = "https://www.thebluealliance.com/api/v2/";//Each request will start with this
+	final String URL_BEGINNING = "https://www.thebluealliance.com/api/v2/", ID_HEADER = "?X-TBA-App-Id=frc5016:scout:v1";//Each request will start with this
 	String currentEventCode;
 	int currentTeam;
 	HttpURLConnection conn;
 	
-	public BlueAllianceConnection(){}
-	
-	public void setEvent(String eventCode){
-		//If is already downloaded, then sync via last modified header
-	}
-	
-	public int[][] getMatches(){
-		//Return a table of the matches for the selected event
-	}
-	
-	public int[] getRanks(){
-		//Return an array of the current ranks for the selected event
-	}
-	
-	public int[][] getTeam(){
+	public BlueAllianceConnection(){
 		
 	}
 	
-	public int[][] getOPR(){
+	public String[] getEventTeamList(String eventCode){
+		
+	}
+	
+	public String[] getEventMatches(){//Gets list of matches and results
+		
+	}
+	public String blueAllianceGet(String address){
+		String response = "-1";
+		try{	
+			URL url = new URL(URL_BEGINNING + address + ID_HEADER);//This is the URL that it will connect to
+			conn = (HttpURLConnection) url.openConnection();
+			conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");//Workaround to make it look like this is a browser
+			response =  Integer.toString(conn.getResponseCode());//ServerCode
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			response = br.readLine();//Data from Server
+		}catch (Exception e) {
+			System.out.println("Connection Error: " + response);
+			response = "-1";
+		}
+		
+		return response;
+		//If response is -1 then there was an error
 		
 	}
 	
